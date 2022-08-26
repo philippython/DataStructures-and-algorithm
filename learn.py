43f8cb10816506904d1652b474ad3981716f8000
odulaja philip <odulajaphilip@gmail.com>
2022-08-26 20:31:49 +0100

    added some concepts

---
+++ learn.py
@@ -0,0 +1,22 @@
+# python datastructure are divided into two built-in and user defined 
+
+"""
+builtin are lists, tuple, dictionary and sets
+
+list are ordered and can be nested and mutable and items can be accessed using index 
+
+tuple are ordered and can be accessed using index they are faster than list 
+
+A tuple with single item can be created using 
+
+new_tuple = (7, )
+
+tuple are immutable and can be nested
+
+Dictionaries are unordered and can be accessed using keys 
+
+dicitionaries are mutable 
+
+it can be looped using dictionaries.items()
+
+"""
