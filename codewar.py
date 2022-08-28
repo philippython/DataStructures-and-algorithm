dadab2fcfe5e525bdce4f582e047042fcf8c3d2f
odulaja philip <odulajaphilip@gmail.com>
2022-08-28 15:08:10 +0100

    sort odd number failed

--- codewar.py
+++ codewar.py
@@ -15,8 +15,57 @@ def to_camel_case(text):
 print(to_camel_case("The-cat_Is_evil"))
 
 def count_bits(n):
-    for num in range(2, n):
-        if n % num == 1:
-            print(num)
+    return bin(n)
+
         
-count_bits(1234)
+print(type(count_bits(3)), count_bits(3))
+
+class PersonType:
+
+    def __init__(self,fn,ln):
+        self.firstName = fn
+        self.lastName = ln
+    def printName(self):
+        print(PersonType.firstName,' ',PersonType.lastName)
+    def setName(self,fn,ln):
+        self.firstName = fn
+        self.lastName = ln
+    def getFirstName(self):
+        return self.firstName
+    def getLastName(self):
+        return self.lastName
+
+class DoctorType(PersonType):
+    def __init__(self, fn, ln):
+        super().__init__(fn, ln)
+        self.speciality = None
+
+    def setSpeciality(self, speciality):
+        self.speciality = speciality
+
+class BillType:
+    def __init__(self, id, charge):
+        self.id = id
+        self.charge = charge
+
+def valid_parentheses(string):
+    #your code here
+    return string.count("(") == string.count(")") and string[::-1].find("(") > string[::-1].find(")")
+
+print(valid_parentheses('yl)(g(ty)(ewc(()isbq)ca)x()gbc'))
+
+def sort_array(source_array):
+    # Return a sorted array.
+    if not source_array : 
+        return []
+    else:
+        for num in source_array:
+            for n in range(0, max(source_array)):
+                if n % 2 == 1 :
+                    print(source_array.index(num))
+                    print(n)
+                    source_array[source_array.index(num)] = n
+            print(source_array)
+        return source_array
+
+sort_array([5, 3, 2, 8, 1, 4])
