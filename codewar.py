7a22949231de408c1a99bd8935f19a95ef0f58e6
odulaja philip <odulajaphilip@gmail.com>
2022-08-28 20:37:35 +0100

    write num in expanded form

--- codewar.py
+++ codewar.py
@@ -86,7 +86,7 @@ def make_readable(seconds):
     
 
 def sum_dig_now(a, b):
-        return [x for x in range(a, b+1) if sum(int(d)**i for i, d in enumerate(str(x), 1)) == x]
+    return [x for x in range(a, b+1) if sum(int(d)**i for i, d in enumerate(str(x), 1)) == x]
 
 #     if seconds >= 60:
 #         if seconds % 3600 == 0 :
