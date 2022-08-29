2b951faa69da9791196a38c12cb730298a8077d6
odulaja philip <odulajaphilip@gmail.com>
2022-08-29 20:04:39 +0100

    oop revision

--- oop.py
+++ oop.py
@@ -1,28 +1,31 @@
 # Creating a Base class
 class Base:
-    def __init__(self):
+    #  class attribute
+    attr1 = 80
+    def __init__(self, city):
         self.a = "GeeksforGeeks"
-        self.__c = "GeeksforGeeks"
+        self.__c = city
  
 # Creating a derived class
 class Derived(Base):
-    def __init__(self):
+    def __init__(self, city):
  
         # Calling constructor of
         # Base class
-        Base.__init__(self)
+        super().__init__(self, city)
         print("Calling private member of base class: ")
-        print(self.__c)
  
  
 # Driver code
-obj1 = Base()
+obj1 = Base("New York")
 print(obj1.a)
  
-print(obj1.c)
+print(obj1.attr1)
 # Uncommenting print(obj1.c) will
 # raise an AttributeError
  
+obj2 = Derived("Toronto")
+print(obj2.__c)
 # Uncommenting obj2 = Derived() will
 # also raise an AtrributeError as
 # private member of base class
