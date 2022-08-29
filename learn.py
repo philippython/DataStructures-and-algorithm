4c45fcb9cc7aa174fcee7ed064653c92d666bb22
odulaja philip <odulajaphilip@gmail.com>
2022-08-29 10:47:49 +0100

    another day to code

--- learn.py
+++ learn.py
@@ -1,3 +1,5 @@
+from random import random
+from webbrowser import get
 from data import data 
 """
 Question 1a.
@@ -23,10 +25,24 @@ def statistics(key, dictionary):
     #     nth = len(actcost_list) / 2 
     #     median = nth + 1 // 2
     # else :
-
+ 
 
     median = len(actcost_list) % 2
 
+rand_list = ["I>E", "M>I", "A>M", "D>A", "E>N"]
+random_dict = { n[0]:n[-1] for n in rand_list}
+print(random_dict)
+def getword(random_dict):
+    new = []
+    for k, v in random_dict.items() :
+        if k not in list(random_dict.values()):
+            new.append(k)
+            random_dict[k] = k
+while len(new) < len(rand_list):
+    getword(random_dict)
+    if len(new) < len(rand_list):
+        print(new)
+        break
 
-
-statistics(8, data)
+getword(random_dict)
+# statistics(8, data)
